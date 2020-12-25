7a48728a7a8e66f22d211ec9df88e6498ec922b9
waldteufelUkrNet <waldteufel@ukr.net>
2020-12-26 00:45:56 +0200

    loader

--- app/modules/loader/loader.js
+++ app/modules/loader/loader.js
@@ -1,8 +1,19 @@
 "use strict";
 // loader module
 ////////////////////////////////////////////////////////////////////////////////
-/* ↓↓↓ FULL SCREEN MODE ON/OFF ↓↓↓ */
-/* ↑↑↑ /FULL SCREEN MODE ON/OFF ↑↑↑ */
+/* ↓↓↓ LOADER ↓↓↓ */
+  let loader = document.querySelector('.loader');
+  let bookInner = document.querySelector('#book');
+
+  bookInner.style.overflow = 'hidden';
+
+  window.onload = () => {
+    setTimeout(function(){
+      loader.classList.remove('loader_active');
+      bookInner.style.overflow = '';
+    },1000);
+  };
+/* ↑↑↑ /LOADER ↑↑↑ */
 ////////////////////////////////////////////////////////////////////////////////
 /* ↓↓↓ FUNCTIONS DECLARATION ↓↓↓ */
 /* ↑↑↑ /FUNCTIONS DECLARATION ↑↑↑ */
